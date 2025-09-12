7e3bee6ebb6aa6d37be9163fe0f8697ae323a7e5
Danilo <danilosoaresmatos@gmail.com>
2025-09-12 16:25:01 -0300

    primeiro grafo

--- src/algorithms/grafo.py
+++ src/algorithms/grafo.py
@@ -1,1 +1,8 @@
-import osmnx 
+import matplotlib
+import osmnx as ox
+
+bairro_de_teste = "Cruz das Almas, Maceió, Alagoas, Brazil"
+
+g = ox.graph_from_place(bairro_de_teste, network_type="all")
+
+fig, ax = ox.plot_graph(g)
